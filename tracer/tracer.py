af2e82511c17abf314a033354696cd63473b1a40
thomas ae smith <thomas.ae.smith@googlemail.com>
2014-03-28 22:24:55 +0000

    Lighting with single light.

--- tracer/tracer.py
+++ tracer/tracer.py
@@ -4,11 +4,23 @@ from PIL import Image
 # debug flags
 CULLING = True
 QUICK_RENDER = False
+SHADING = True
 
 # shorthand
 def vec(a, b, c):
 	return numpy.array([a, b, c])
 
+# use numpy to normalise a vector
+def normalise(v):
+	v /= numpy.linalg.norm(v)
+	return v
+
+# Calculate half-vector
+def H(camera, light):
+	# print camera, light
+	return normalise(normalise(camera) + normalise(light))
+
+# load an .obj file vertices and apply uniform colour
 def readobj(filename, name, colour):
 	obj = {}
 	points = []
@@ -32,7 +44,6 @@ def readobj(filename, name, colour):
 	obj['name'] = name
 	obj['centre'] = vec(numpy.mean([x[0] for x in points]), numpy.mean([x[1] for x in points]), numpy.mean([x[2] for x in points]))
 	obj['radius'] = max(map(numpy.linalg.norm, [x - obj['centre'] for x in points]))
-	# print obj
 	return obj
 
 
@@ -41,68 +52,50 @@ def readobj(filename, name, colour):
 # cube = 'Trns_shift( [2 0.5 1] )';		*2?
 # plane = 'Trns_xrot(  1.5708 )';		*15?
 # window = [256 256 1 1]; % you can define the window differently if you like
-# ambient_light = [0.3 0.3 0.3]; % Use this as a default 
-# directional_light_1 = [5 5 -5]; % Use this basic light for this scene - you can alter if you like 
-# directional_light_2 = [-5 5 -5]; % Use this basic light for this scene - you can alter if you like
-# example_camera = [0 3 -10]; % Here is a basic camera - you can also alter this if you like.
 
 objects = [{}]
-# objects[0] = {'name': "plane", 'colour': (128, 0, 0), 'tri': [[ vec(1, 1, 0), vec(-1, -1, 0),vec(-1, 1, 0)]]}
-# objects[0] = {'name': "plane", 'colour': (128, 0, 0), 'tri': [[vec(1, 1, 0), vec(1, -1, 0), vec(-1, -1, 0)]]}
-objects[0] = {'name': "plane", 'colour': (128, 0, 0), 'tri': [[vec(1, 1, 0), vec(-1, -1, 0), vec(-1, 1, 0)],[vec(1, 1, 0), vec(1, -1, 0), vec(-1, -1, 0)]], 'centre': vec(0, 0, 0,), 'radius': numpy.sqrt(2.0)}
-objects.append(readobj('cube.obj', 'cube', (0, 0, 256)))
-objects.append(readobj('cylinder.obj', 'cylinder', (0, 0, 256)))
-objects.append(readobj('plane.obj', 'plane', (128, 128, 0)))
-print objects
-# scene.objects: This contains some number of objects in the scene. Double click on this, and then one of the cells to view its properties. We talked about these in Lab Sheet 2, but a quick refresher:
-#  object.x: Ignore, gives original points, use transformed points instead.
-#  object.p: Transformed points. Each row is a point in 3D space.
-#  object.q: Ignore, gives mid-way points before final transformations.
-#  object.tri: Each row is a triangle in this object, lists three points in object.p.
+objects[0] = {'name': "plane", 'colour': vec(128, 0, 0), 'tri': [[vec(1, 1, 0), vec(-1, -1, 0), vec(-1, 1, 0)],[vec(1, 1, 0), vec(1, -1, 0), vec(-1, -1, 0)]], 'centre': vec(0, 0, 0,), 'radius': numpy.sqrt(2.0)}
+objects.append({'name': "sphere", 'colour': vec(0, 128, 128), 'tri': [], 'centre': vec(-4, 4, 2), 'radius': 2.0})
+objects.append(readobj('cube.obj', 'cube', vec(0, 0, 256)))
+objects.append(readobj('cylinder.obj', 'cylinder', vec(0, 0, 256)))
+objects.append(readobj('plane.obj', 'plane', vec(128, 128, 0)))
 #  object.colour: Each row gives the colour of the corresponding triangle. First three columns are RGB values. Final two are a reflectance and refraction index for full ray-tracing.
 #  object.n: The normal for each triangle.
-#  object.name: A human-readable string describing the shape of the object.
-#  scene.numberofobjects: The total number of objects, equal to the length of the object field, for
-# easy reference.
 
-#  scene.imagesize: The resolution for this render, given as [width, height].
+# The resolution for this render
 image_width = 360
 image_height = 200
-#  scene.windowsize: The size of the image plane (in the 3D space), given as [width, height]. 
+# The size of the image plane (in the 3D space)
 window_width = image_width/image_height
 window_height = 1
-#  scene.ambientlight: The values to use for the ambient light, RGB.
-ambientlight = (0.3, 0.3, 0.3)
-#  scene.directionallight: Each row contains the location of a light in 3D space.
+# The values to use for the ambient light, RGB.
+ambientlight = vec(0.3, 0.3, 0.3)
+Kd = 2
+Ks = 25
+p = 10
+# Directional light locations
 lights = [vec(5, 5, -5), vec(-5, 5, -5)]
-#  scene.cam: The camera definition to use. Has the following fields:
-#  cam.focus: The focal point of the camera in 3D space.
-#  cam.focallength: The distance between the focal point and the image plane.
-#  cam.{up,right,normal}: The orientation of the camera.
-#  cam.coi: The centre of the image plane, computed from the other values for easy reference.
+# The focal point of the camera in 3D space.
 cam_focus = vec(0, 3, -10)
+# The distance between the focal point and the image plane.
 cam_focallength = 1
+# The orientation of the camera.
 cam_up = vec(0, 1, 0)
 cam_right = vec(1, 0, 0)
 cam_normal = vec(0, 0, 1)
+# The centre of the image plane, computed from the other values for easy reference.
 cam_coi = cam_focus + cam_normal * cam_focallength
 cam_tlp = cam_coi - cam_right * 0.5 * window_width + cam_up * 0.5 * window_height
-print cam_coi
-print cam_tlp
 
-# returns the world coordingates of a point on the image plane
+# returns the world coordinates of a point on the image plane
 # where x and y are planespace coordinates [0-r] and [0-1]
 def getWindowPoint(x, y):
 	return cam_tlp + x * cam_right - y * cam_up
 
-# use numpy to normalise a vector
-def normalise(v):
-	v /= numpy.linalg.norm(v)
-	return v
 
 def intersect(origin, ray, obj):
 	# initialise a collision record
-	c_rec = {'hit': False}
+	c_rec = {'hit': False, 'colour': obj['colour']}
 	# initialise t to maximum possible
 	t = numpy.inf
 
@@ -121,13 +114,14 @@ def intersect(origin, ray, obj):
 		t2 = dsr - d
 		if t2 > t: return c_rec
 
-		# if rendering only culling spheres
-		if (QUICK_RENDER):
+		# if no faces defined (it's a sphere) or rendering only culling spheres
+		if (obj['tri'] == [] or QUICK_RENDER):
 			if t2 < 0:
 				t = t1
 			else:
 				t = t2
-			return t
+			c_rec = {'hit': True, 't': t, 'colour': obj['colour'], 'norm': normalise(sr - (t*ray))}
+			return c_rec
 
 	# print "checking", obj['name']
 	for i, points in enumerate(obj['tri']):
@@ -168,7 +162,7 @@ def intersect(origin, ray, obj):
 # trace a numpy vector
 def trace(origin, ray):
 	t = numpy.inf
-	c_rec = {}
+	c_rec = {'hit': False}
 
 	for i, obj in enumerate(objects):
 		coll = intersect(origin, ray, obj)
@@ -177,12 +171,23 @@ def trace(origin, ray):
 				t = c_rec['t']
 				c_rec['obj_id'] = i
 
-	if t == numpy.inf:
-		return (0, 128, 0)
+	# if we've failed to hit anything, return a background colour
+	if c_rec['hit'] == False:
+		return vec(0, 128, 0)
 
-		
-	# print 'colour is ',  objects[coll_id]['colour']
-	return c_rec['colour']
+	if not SHADING:
+		return c_rec['colour']
+
+	La = ambientlight * c_rec['colour'] 
+
+	Ld = Kd * vec(1,1,1) * max(0, numpy.dot(c_rec['norm'], (c_rec['t'] * ray) - lights[0]))
+	h = H(ray, (c_rec['t'] * ray) - lights[0])
+	Ls = Ks * vec(1,1,1) * numpy.power(max(0, numpy.dot(c_rec['norm'], h)), p)
+
+	# print (La + Ld + Ls)
+	return numpy.clip(La + Ld + Ls, 0, 256)
+
+
 
 
 image = Image.new("RGB", (image_width, image_height))
@@ -198,9 +203,6 @@ for i, x in enumerate(numpy.linspace(0, window_width, image_width)):
 		ray = getWindowPoint(x, y) - cam_focus
 		ray = normalise(ray)
 		colour = trace(cam_focus, ray)
-		# print i, j, " = ", colour
-		# raw[i,j] = (int(ray[0]*256)+128, int(ray[1]*256+128), 0)
-		raw[i,j] = colour
-		# raw[x,y] = (int((y*256.0)/h), int((x*256.0)/w), 256)
+		raw[i,j] = tuple(colour.astype(int))
 
 image.save("imgs/scene.png")
